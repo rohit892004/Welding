1fc164feba002c9d23ed9e0fb717f58c6f2a83f9
Manichandra <manichandra.vallala@gmail.com>
2025-11-03 13:44:46 +0530

    Mainmenu Integration

---
+++ NEW_Welding/Assets/SceneChanger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void Homepage()
+    {
+        SceneManager.LoadScene("MainMenu_Scene");
+    }
+
+    public void TIGEnglish()
+    {
+        SceneManager.LoadScene("TIG_WELDING_SCENE_English");
+    }
+
+    public void TIGHinid()
+    {
+        SceneManager.LoadScene("TIG_WELDING_SCENE_Hindi");
+
+    }
+
+    public void MIGEnglish()
+    {
+        SceneManager.LoadScene("MIG_WELDING_SCENE_English");
+    }
+
+    public void MIGHindi()
+    {
+        SceneManager.LoadScene("MIG_WELDING_SCENE_Hindi");
+    }
+}
